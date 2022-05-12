c3eacc1b27c254b7bc49cacfd3cf6fc212325ac4
Lahav_Padan <lahavp10@gmail.com>
2022-05-12 22:21:56 +0300

    Cleaning up

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -33,28 +33,3 @@ def create():
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
-
-
-# @app.route('/add', methods=['POST'])
-# def add():
-#     data = request.get_json()
-#     name = data['name']
-#     price = data['price']
-#     breed = data['breed']
-
-#     db.add_instance(Cats, name=name, price=price, breed=breed)
-#     return json.dumps("Added"), 200
-
-
-# @app.route('/remove/<cat_id>', methods=['DELETE'])
-# def remove(cat_id):
-#     db.delete_instance(Cats, id=cat_id)
-#     return json.dumps("Deleted"), 200
-
-
-# @app.route('/edit/<cat_id>', methods=['PATCH'])
-# def edit(cat_id):
-#     data = request.get_json()
-#     new_price = data['price']
-#     db.edit_instance(Cats, id=cat_id, price=new_price)
-#     return json.dumps("Edited"), 200
